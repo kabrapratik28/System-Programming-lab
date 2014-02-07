48a5df8bab36ff9e18f51f10554dd64396e8c659
kabrapratik28 <kabrapratik28@gmail.com>
2014-02-07 21:33:06 +0530

    completed assembler ooo output

--- tut2 Assembler/a.asm
+++ tut2 Assembler/a.asm
@@ -1,7 +1,7 @@
 start 2000
 mover a,m
 movm c,n
-loop: add,='5'
+loop: add c,='5'
 mover b,='9'
 mult c,m
 movem b,n
--- tut2 Assembler/a.cpp
+++ tut2 Assembler/a.cpp
@@ -61,7 +61,7 @@ public:
 class FRT {
 public:
   vector<string> symbol; 
-  //vector<string> addofdef; 
+  vector<int> addofdef; 
   vector<vector <int> > addofuse; 
 
   void addelement (string element , int location)
@@ -76,6 +76,27 @@ public:
       {
 	symbol.push_back (element) ; // else add .. and create new vector add in vector
 	vector<int> locations (1,location) ; 
+	addofuse.push_back (locations);
+	addofdef.push_back (0) ; 
+      }
+    
+    
+  }
+
+
+  void addelement_with_add (string element , int addloct)
+  {
+    int curr_posi ; 
+    curr_posi = elementfinder(element);     // element finder class using
+    if (curr_posi != -1)// check present or not
+      {
+	addofdef.at(curr_posi) = addloct ;
+      }
+    else 
+      {
+	symbol.push_back (element) ; // else add .. and create new vector add in vector
+	addofdef.push_back (addloct);
+	vector<int> locations ; 
 	addofuse.push_back (locations); 
       }
     
@@ -149,69 +170,135 @@ int addressinc ()
   return address_starter ; 
 }
 
-int startchecker(string onelineofcode)
+vector<string> tokengiver(string com_sen)
+{
+  vector<string> tokens ; 
+  istringstream is_in (com_sen);  // string stream
+  string words ; 
+  while(is_in >> words) {
+    tokens.push_back(trim(words)) ;	
+  }
+
+  return tokens ; 
+}
+
+void frtadder(vector<string> frtadd)
+{
+  for(std::vector<string>::iterator itera = frtadd.begin(); itera != frtadd.end(); ++itera) {
+    if ((*itera != "a") && (*itera != "b") && (*itera != "c") && (*itera != "d")){
+      frt.addelement ( *itera ,address_starter );
+    }
+  }
+}
+
+void startchecker(string onelineofcode)
 {
   // 1. move 2.mover 3.movem 4.mult 5.bc 6.stop 7.end 8.start 9.add 10.sub 11.jmp 12.ltorg 13.comp 14. equ
-  
-  if (onelineofcode.compare(0, move.length(), move) == 0)
+  vector<string> tokensgot ;
+  //order imp of mover movem and move
+   if (onelineofcode.compare(0, mover.length(), mover) == 0)
     {
-      return 1 ; 
-    }
-  else if (onelineofcode.compare(0, mover.length(), mover) == 0)
-    {
-      return 2 ;
+      onelineofcode.erase (0,mover.length());
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, movem.length(), movem) == 0)
     {
-      return 3 ;
+      onelineofcode.erase (0,movem.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
+    }
+  else if (onelineofcode.compare(0, move.length(), move) == 0)
+    {
+      onelineofcode.erase (0,move.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, mult.length(), mult) == 0)
     {
-      return 4 ;
+      onelineofcode.erase (0,mult.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, bc.length(),bc) == 0)
     {
-      return 5 ;
+      onelineofcode.erase (0,bc.length()); 
+      string nonsense  ; // for bc i.e.for e.g. less than (le)
+      istringstream is_nonse (onelineofcode);  // string stream
+      is_nonse >> nonsense ;    // remove than conditional
+      string words ;                    
+      vector<string> tokens ;
+      while(is_nonse >> words) {               // above .str() also take le 
+    tokens.push_back(trim(words)) ;	
+      }
+      frtadder(tokens) ; 
     }
   else if (onelineofcode.compare(0, stop.length(),stop) == 0)
     {
-      return 6 ;
+      // do nothing 
     }
   else if (onelineofcode.compare(0, end.length(), end) == 0)
     {
-      return 7 ;
+      // do nothing 
     }
   else if (onelineofcode.compare(0, start.length(), start) == 0)
     {
-      return 8 ;
+      onelineofcode.erase (0,start.length());
+      istringstream addr_str (onelineofcode);  // string stream     ..... //set address
+      addr_str >> address_starter ; 
     }
   else if (onelineofcode.compare(0, add.length(), add) == 0)
     {
-      return 9 ;
+      onelineofcode.erase (0,add.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, sub.length(), sub) == 0)
     {
-      return 10 ;
+      onelineofcode.erase (0,sub.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, jmp.length(), jmp) == 0)
     {
-      return 11 ;
+      onelineofcode.erase (0,jmp.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, ltorg.length(), ltorg) == 0)
     {
-      return 12 ;
+      // do nothing 
     }
   else if (onelineofcode.compare(0, comp.length(), comp) == 0)
     {
-      return 13 ;
+      onelineofcode.erase (0,comp.length());  
+      tokensgot = tokengiver(trim(onelineofcode)) ;
+      frtadder(tokensgot) ; 
     }
   else if (onelineofcode.compare(0, equ.length(), equ) == 0)
     {
-      return 14 ;
+      lt.addelement (onelineofcode , address_starter );
+      vector<string> justone(1,onelineofcode);       
+      frtadder(justone) ;
     }
   else 
     {
-      return 15 ; 
+      // ds and dc storage and defination
+      istringstream dcdm (onelineofcode);  // string stream     ..... //dcdm
+      string oper , dd  ; 
+      int val ; 
+      dcdm >> oper ; 
+      dcdm >> dd ; 
+      dcdm >> val ; 
+      if (dd == "dc")
+	{
+	  st.addelement (oper,address_starter,val , 0);
+	  frt.addelement_with_add (trim(oper) ,address_starter ) ; 
+	}
+      else if (dd == "ds"){
+	st.addelement (oper , address_starter, 0, val);
+	  frt.addelement_with_add (trim(oper) ,address_starter ) ; 
+      }
     }
 }
 
@@ -221,10 +308,8 @@ int main()
 	
   ifstream file("a.asm");   //open file
   string line_by_line;     //line by line read
-  string words ;           // word
   string clean_string,clean_string1, instru ;
-  vector<string> labelstorer ;
-  istringstream is_in; 
+  vector<string> labelstorer ; 
   address_starter = 1000 ;   // by default
   while (getline(file, line_by_line))   //get line
     {
@@ -240,7 +325,7 @@ int main()
 	{
 	  instru = trim(labelstorer.at(labelstorer.size()-1)) ; 
 	  for(std::vector<string>::size_type elela = 0; elela != labelstorer.size()-1; elela++) {
-	    lt.addelement ( trim(labelstorer.at(elela)),address_starter );
+	    frt.addelement_with_add (trim(labelstorer.at(elela)) ,address_starter ) ; 
 	  }
 	}
       else
@@ -250,22 +335,51 @@ int main()
       //label processing ends here ================================
 
       //check instruction and do operations 
-      
+     
+      startchecker(instru) ; 
 
-      is_in (instru);  // string stream
-      
-      while(iss >> words) {
-      cout << words ;	
-      }
-
-
-
+      address_starter =   addressinc ();
 
     }
   int loc_con =  0 ;
-    while (loc_con<=lt.number.size()-1)
+  cout << "             LITREAL TABLE             \n" ; 
+  while (loc_con<=lt.number.size()-1)
     {
       cout << lt.number[loc_con] << "  " << lt.literal[loc_con]<< " "<< lt.location[loc_con]<<"\n" ; 
+      loc_con++ ;   
+    }
+  
+ cout << "\n\n\n"  ; 
+  loc_con =  0 ;
+  cout << "             FORWARD REVERSE TABLE             \n" ; 
+  while (loc_con<=frt.symbol.size()-1)
+    {
+
+      string maker("") ; 
+  int tato = 0  ; 
+  
+  while (tato<=frt.addofuse.at(loc_con).size()-1)
+    {
+     
+stringstream ss;
+ ss << frt.addofuse.at(loc_con).at(tato) ; 
+      maker = maker + ss.str()+" ";
+      tato++ ; 
+    }
+
+  stringstream tatkkk ; 
+   tatkkk << frt.addofdef[loc_con] ; 
+
+   cout << frt.symbol[loc_con] << "  " <<tatkkk.str() << " "<< maker<<"\n" ; 
+      loc_con++ ;   
+    }
+  
+  cout << "\n\n\n"  ; 
+  loc_con =  0 ;
+  cout << "             SYMBOL TABLE             \n" ; 
+  while (loc_con<=st.name.size()-1)
+    {
+      cout << st.name[loc_con] << "  " << st.address[loc_con]<< " "<< st.value[loc_con]<< st.lenght[loc_con]<<"\n" ; 
       loc_con++ ;   
     }
   
